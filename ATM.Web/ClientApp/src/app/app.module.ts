b85d4efca1501b80fb9f748ad10251ab23dab0a7
JC <accgo3241@gmail.com>
2022-09-23 10:06:42 -0500

    Validación campo monto

--- ATM.Web/ClientApp/src/app/app.module.ts
+++ ATM.Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {InjectionToken, NgModule} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -13,6 +13,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {TransactionService} from "./services/TransactionService";
 import {MatBadgeModule} from "@angular/material/badge";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatChipsModule} from "@angular/material/chips";
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {MatButtonModule} from "@angular/material/button";
 
 
 export const BASE_API = new InjectionToken<string>('base_url', {
@@ -27,7 +33,7 @@ export const BASE_API = new InjectionToken<string>('base_url', {
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
@@ -40,7 +46,14 @@ export const BASE_API = new InjectionToken<string>('base_url', {
     ]),
     BrowserAnimationsModule,
     MatTabsModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatChipsModule,
+    DragDropModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [TransactionService],
   bootstrap: [AppComponent]
--- ATM.Web/ClientApp/src/app/home/home.component.ts
+++ ATM.Web/ClientApp/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TransactionService} from "../services/TransactionService";
 import {Bill} from "../models/Bill";
 import {catchError} from "rxjs";
+import {AbstractControl, FormControl, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,10 @@ export class HomeComponent implements OnInit{
 
   public amounts: Bill[] = [];
   public loadingAmount = false;
+  public withdrawAmounts = [20000, 50000, 100000, 200000, 400000, 700000];
+
+  public amountControl: FormControl = new FormControl<number>(0, [Validators.required, HomeComponent.AmountValidator]);
+
 
   constructor(public service: TransactionService) {
   }
@@ -30,4 +35,30 @@ export class HomeComponent implements OnInit{
   get totalAmount(){
     return this.amounts.reduce((p, x) => p + x.amount, 0);
   }
+
+
+  withdraw(){
+    if(this.amountControl.valid) {
+      console.log("khkkh")
+    } else {
+      console.log(("oljdpn"))
+    }
+  }
+
+  selectValue(value: number){
+    this.amountControl.setValue(value);
+  }
+
+  getErrorMessage() {
+    if (this.amountControl.hasError('required')) {
+      return 'You must enter a value';
+    }
+    let invalidAmount = this.amountControl.hasError('amountInvalid');
+    return invalidAmount ? this.amountControl.getError('amountInvalid') : '';
+  }
+
+  static AmountValidator(control: AbstractControl): ValidationErrors | null {
+    return control.value % 10000 ? {amountInvalid: "Solo multiplos de 10.000"} : null;
+  }
+
 }
